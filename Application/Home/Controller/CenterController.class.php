92240dcc7002007fcc0e3d83c2405354ee96839c
Administrator <Administrator@20160923-100837>
2017-02-10 11:33:02 +0800

    个人中心页面

---
+++ Application/Home/Controller/CenterController.class.php
@@ -0,0 +1,14 @@
+<?php
+	namespace Home\Controller;
+	use Think\Controller;
+	class CenterController extends Controller {
+		public function index(){
+		    // 显示用户购物车购物数量
+		    $this -> assign("shopNum", shopCartNum($_SESSION['user']['uid']));
+		    // 显示导航栏
+			$this -> assign("list", displayFirst());
+        	$this -> assign("result", displaySecond());
+        	$this -> assign("username", $_SESSION['user']['username']." 欢迎回来");
+			$this -> display("index");
+		}
+	}
