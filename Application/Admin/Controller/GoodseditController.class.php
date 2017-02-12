a685cff698314a9cfd5bd75d744821e78d4019ad
YangSen <YangSen@YangSen>
2017-02-12 14:10:30 +0800

    商品修改

--- Application/Admin/Controller/GoodseditController.class.php
+++ Application/Admin/Controller/GoodseditController.class.php
@@ -55,42 +55,6 @@
             $this->error("修改失败", "edit?id={$_GET['id']}");
             }
     }
-        // public function update() {
-        //     $data['pid'] = $_POST['pid'];
-        //     $data['title'] = $_POST['title'];
-        //     $data['stock'] = $_POST['stock'];
-        //     $data['inputer'] = $_SESSION['admin']['username'];
-        //     $data['price'] = $_POST['price'];
-        //     $data['preprice'] = $_POST['preprice'];
-        //     $inf['typeid'] = $_POST['typeid'];
-        //     $inf['brandid'] = $_POST['brandid'];
-        //     $inf['hot'] = $_POST['hot'];
-        //     $inf['depreciate'] = $_POST['depreciate'];
-        //     $inf['commend'] = $_POST['commend'];
-        //     $inf['discount'] = $_POST['discount'];
-        //     $inf['offsale'] = $_POST['offsale'];
-        //     $inf['hits'] = 0;
-        //     $inf['picurl'] = $_POST['picurl']; 
-        //     $inf['picurls'] = $_POST['picurls'];
-        //     $inf['contents'] = $_POST['contents'];
-        //     //dump($data['pid']);
-        //     $mod = M('product');
-        //     $result = $mod -> find($data['pid']);
-        //     //$result['id'] = $_POST['id'];
-        //      if(($mod->save()))
-        //     {
-        //     $this->success("修改成功", "../Goodsupdate/index");
-        //     }else{
-        //     $this->error("修改失败", "edit?id={$_GET['id']}");
-        //     }
-        //     $mod1 = M('products_details');
-        //     $res = $mod1 ->find($data['pid']);
-        //     if(($mod1->save()))
-        //     {
-        //     $this->success("修改成功", "../Goodsupdate/index");
-        //     }else{' '
-        //     $this->error("修改失败", "edit?id={$_GET['id']}");
-        //     }
-        //   }
+       
         }
     
